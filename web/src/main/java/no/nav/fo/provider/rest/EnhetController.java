6ee7650baabeb991e0c80c2ce8fc346b351189d1
Christian Finstad <christian.finstad@nav.no>
2017-01-30 09:06:03 +0100

    Returnerer en liste med alle ressurser tilknyttet enheten.

--- web/src/main/java/no/nav/fo/provider/rest/EnhetController.java
+++ web/src/main/java/no/nav/fo/provider/rest/EnhetController.java
@@ -2,6 +2,9 @@ package no.nav.fo.provider.rest;
 
 
 import no.nav.fo.service.VirksomhetEnhetServiceImpl;
+import no.nav.virksomhet.tjenester.enhet.meldinger.v1.WSHentRessursListeResponse;
+import no.nav.virksomhet.tjenester.enhet.v1.HentRessursListeEnhetikkefunnet;
+import no.nav.virksomhet.tjenester.enhet.v1.HentRessursListeUgyldigInput;
 
 import javax.inject.Inject;
 import javax.ws.rs.GET;
@@ -11,6 +14,9 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
+import static javax.ws.rs.core.Response.Status.BAD_REQUEST;
+import static javax.ws.rs.core.Response.Status.INTERNAL_SERVER_ERROR;
+import static javax.ws.rs.core.Response.Status.NO_CONTENT;
 
 @Path("/enhet")
 @Produces(APPLICATION_JSON)
@@ -23,6 +29,18 @@ public class EnhetController {
     @Path("/{enhetId}/veiledere")
     public Response hentRessurser(@PathParam("enhetId") String enhetId) {
 
-        return Response.ok().entity(enhetId).build();
+        WSHentRessursListeResponse response = null;
+
+        try {
+            response = virksomhetEnhetService.hentRessursListe(enhetId);
+        } catch (HentRessursListeUgyldigInput e) {
+            return Response.status(BAD_REQUEST).build();
+        } catch (HentRessursListeEnhetikkefunnet e) {
+            return Response.status(NO_CONTENT).build();
+        } catch (Exception e) {
+            return Response.status(INTERNAL_SERVER_ERROR).build();
+        }
+
+        return Response.ok().entity(response).build();
     }
 }
