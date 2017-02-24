e6114332517a010c4ed158b4c300bb79068b21d5
Hein Haraldsen <hein.haraldsen@nav.no>
2017-02-24 12:37:44 +0100

    PK-37069 PKULV-2218 Henter saksbehandlers navn rett ut fra enhetslista

--- web/src/main/java/no/nav/fo/domain/Saksbehandler.java
+++ web/src/main/java/no/nav/fo/domain/Saksbehandler.java
@@ -3,12 +3,11 @@ package no.nav.fo.domain;
 public class Saksbehandler {
 
     private String ident;
+    private String navn;
 
     public void setNavn(String navn) {
         this.navn = navn;
     }
-
-    private String navn;
 
     public Saksbehandler withIdent(String ident) {
         this.ident = ident;
--- web/src/main/java/no/nav/fo/service/VirksomhetEnhetServiceImpl.java
+++ web/src/main/java/no/nav/fo/service/VirksomhetEnhetServiceImpl.java
@@ -2,7 +2,6 @@ package no.nav.fo.service;
 
 
 import no.nav.fo.domain.Saksbehandler;
-import no.nav.virksomhet.organisering.enhetogressurs.v1.Ressurs;
 import no.nav.virksomhet.tjenester.enhet.meldinger.v1.WSHentEnhetListeRequest;
 import no.nav.virksomhet.tjenester.enhet.meldinger.v1.WSHentEnhetListeResponse;
 import no.nav.virksomhet.tjenester.enhet.meldinger.v1.WSHentRessursListeRequest;
@@ -16,7 +15,6 @@ import org.slf4j.Logger;
 import org.springframework.cache.annotation.Cacheable;
 
 import javax.inject.Inject;
-import java.util.List;
 
 import static org.slf4j.LoggerFactory.getLogger;
 
@@ -39,43 +37,35 @@ public class VirksomhetEnhetServiceImpl {
             logger.error(feil, e);
             throw e;
         } catch (HentEnhetListeRessursIkkeFunnet e) {
-            String feil = String.format("Kunne ikke hente ansattopplysnigner for %s", ident);
+            String feil = String.format("Kunne ikke hente ansattopplysninger for %s", ident);
             logger.error(feil, e);
             throw e;
         } catch (java.lang.Exception e) {
-            String feil = String.format("Kunne ikke hente ansattopplysnigner for %s: Ukjent Feil", ident);
+            String feil = String.format("Kunne ikke hente ansattopplysninger for %s: Ukjent Feil", ident);
             logger.error(feil, e);
             throw e;
         }
     }
 
-    public Saksbehandler hentSaksbehandlerInfo(String ident, String enhetId) throws Exception {
-        String saksbehandlernavn = "";
+    public Saksbehandler hentSaksbehandlerInfo(String ident) throws Exception {
+
         Saksbehandler saksbehandler = new Saksbehandler().withIdent(ident);
         try {
-            WSHentRessursListeRequest request = new WSHentRessursListeRequest();
-            request.withEnhetId(enhetId);
-            WSHentRessursListeResponse response = virksomhetEnhet.hentRessursListe(request);
-            List<Ressurs> ressurser = response.getRessursListe();
-            for (Ressurs ressurs : ressurser) {
-                if (ressurs.getRessursId().equals(ident)) {
-                    saksbehandlernavn = ressurs.getNavn();
-                    saksbehandler.setNavn(saksbehandlernavn);
-                    return saksbehandler;
-                }
-            }
+            WSHentEnhetListeRequest request = new WSHentEnhetListeRequest();
+            request.setRessursId(ident);
+            WSHentEnhetListeResponse response = virksomhetEnhet.hentEnhetListe(request);
+            saksbehandler.setNavn(response.getRessurs().getNavn());
             return saksbehandler;
-
-        } catch (HentRessursListeUgyldigInput e) {
-            String feil = String.format("Kunne ikke hente ressursliste for %s", enhetId);
+        } catch (HentEnhetListeUgyldigInput e) {
+            String feil = String.format("Kunne ikke hente ansattopplysnigner for %s", ident);
             logger.error(feil, e);
             throw e;
-        } catch (HentRessursListeEnhetikkefunnet e) {
-            String feil = String.format("Kunne ikke hente ressursliste for %S", enhetId);
+        } catch (HentEnhetListeRessursIkkeFunnet e) {
+            String feil = String.format("Kunne ikke hente ansattopplysninger for %s", ident);
             logger.error(feil, e);
             throw e;
-        } catch (Exception e) {
-            String feil = String.format("Kunne ikke hente saksbehandler for %s", ident);
+        } catch (java.lang.Exception e) {
+            String feil = String.format("Kunne ikke hente ansattopplysninger for %s: Ukjent Feil", ident);
             logger.error(feil, e);
             throw e;
         }
