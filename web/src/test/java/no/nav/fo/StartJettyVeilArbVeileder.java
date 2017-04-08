29f8a5b110a1b76f8a7a8c5cf64aa8ebbe01f9d7
Christian Finstad <christian.finstad@nav.no>
2017-04-08 10:05:06 +0200

    Setter innlogget bruker ved lokal kjøring

--- web/src/test/java/no/nav/fo/StartJettyVeilArbVeileder.java
+++ web/src/test/java/no/nav/fo/StartJettyVeilArbVeileder.java
@@ -1,6 +1,6 @@
 package no.nav.fo;
 
-import no.nav.brukerdialog.security.context.JettySubjectHandler;
+import no.nav.brukerdialog.security.context.InternbrukerSubjectHandler;
 import no.nav.sbl.dialogarena.common.jetty.Jetty;
 import no.nav.sbl.dialogarena.test.SystemProperties;
 import org.apache.geronimo.components.jaspi.AuthConfigFactoryImpl;
@@ -18,9 +18,12 @@ public class StartJettyVeilArbVeileder {
     public static void main(String[] args) {
         SystemProperties.setFrom("jetty-veilarbveileder.properties");
         System.setProperty("develop-local", "true");
-        setProperty("no.nav.modig.core.context.subjectHandlerImplementationClass", JettySubjectHandler.class.getName());
         System.setProperty("org.apache.geronimo.jaspic.configurationFile", "src/test/resources/jaspiconf.xml");
         Security.setProperty(AuthConfigFactory.DEFAULT_FACTORY_SECURITY_PROPERTY, AuthConfigFactoryImpl.class.getCanonicalName());
+
+        InternbrukerSubjectHandler.setVeilederIdent("***REMOVED***");
+        InternbrukerSubjectHandler.setServicebruker("***REMOVED***");
+        setProperty("no.nav.modig.core.context.subjectHandlerImplementationClass", InternbrukerSubjectHandler.class.getName());
 
         //Må ha https for csrf-token
         final Jetty jetty = Jetty.usingWar()
