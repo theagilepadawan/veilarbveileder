c13c7a6d91e236531a6170b0c0e0e05840b873cf
Christian Finstad <christian.finstad@nav.no>
2017-04-12 15:27:28 +0200

    Legger til timer på isSubjectMemberOfModiaOppfolging

--- web/src/main/java/no/nav/fo/service/PepClient.java
+++ web/src/main/java/no/nav/fo/service/PepClient.java
@@ -1,5 +1,7 @@
 package no.nav.fo.service;
 
+import no.nav.metrics.MetricsFactory;
+import no.nav.metrics.Timer;
 import no.nav.sbl.dialogarena.common.abac.pep.Pep;
 import no.nav.sbl.dialogarena.common.abac.pep.domain.response.BiasedDecisionResponse;
 import no.nav.sbl.dialogarena.common.abac.pep.domain.response.Decision;
@@ -23,7 +25,11 @@ public class PepClient implements PepClientInterface {
     public boolean isSubjectMemberOfModiaOppfolging(String ident) {
         BiasedDecisionResponse callAllowed;
         try {
+            Timer timer = MetricsFactory.createTimer("isSubjectMemberOfModiaOppfolging");
+            timer.start();
             callAllowed  = pep.isSubjectMemberOfModiaOppfolging(ident);
+            timer.stop();
+            timer.report();
         } catch (PepException e) {
             throw new InternalServerErrorException("Something went wrong when wrong in PEP", e);
         }
