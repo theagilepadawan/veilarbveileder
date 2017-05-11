328144ddcaa3239629c5b18add1628e89bdf7d4a
Christian Finstad <christian.finstad@nav.no>
2017-05-11 12:16:34 +0200

    Skal gi tilgang for alle enheter dersom ingen er definert i pilotliste

--- web/src/main/java/no/nav/fo/provider/rest/EnhetController.java
+++ web/src/main/java/no/nav/fo/provider/rest/EnhetController.java
@@ -3,6 +3,7 @@ package no.nav.fo.provider.rest;
 
 import io.swagger.annotations.Api;
 import no.nav.brukerdialog.security.context.SubjectHandler;
+import no.nav.fo.Utils;
 import no.nav.fo.domene.VeiledereResponse;
 import no.nav.fo.service.PepClientInterface;
 import no.nav.fo.service.VirksomhetEnhetService;
@@ -15,8 +16,6 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.Response;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 import static javax.ws.rs.core.Response.Status.*;
@@ -37,6 +36,12 @@ public class EnhetController {
     public Response hentRessurser(@PathParam("enhetId") String enhetId) {
         String ident = SubjectHandler.getSubjectHandler().getUid();
 
+        if(!Utils.enhetErIPilot(enhetId)) {
+            Enhet returnEnhet = new Enhet();
+            returnEnhet.setEnhetId(enhetId);
+            return Response.ok().entity(new VeiledereResponse().setVeilederListe(new ArrayList<>()).setEnhet(returnEnhet)).build();
+        }
+
         try {
             boolean isUserInModigOppfolging = pepClient.isSubjectMemberOfModiaOppfolging(ident);
             if(!isUserInModigOppfolging) {
@@ -46,17 +51,9 @@ public class EnhetController {
             return Response.status(INTERNAL_SERVER_ERROR).build();
         }
 
-        List<String> enheterIPilot = Arrays.asList(System.getProperty("portefolje.pilot.enhetliste").split(","));
-
-
         VeiledereResponse response = null;
 
         try {
-            if(!enheterIPilot.contains(enhetId)) {
-                Enhet returnEnhet = new Enhet();
-                returnEnhet.setEnhetId(enhetId);
-                return Response.ok().entity(new VeiledereResponse().setVeilederListe(new ArrayList<>()).setEnhet(returnEnhet)).build();
-            }
             response = virksomhetEnhetService.hentRessursListe(enhetId);
         } catch (HentRessursListeUgyldigInput e) {
             return Response.status(BAD_REQUEST).build();
---
+++ web/src/test/java/no/nav/fo/provider/rest/EnhetControllerTest.java
@@ -0,0 +1,56 @@
+package no.nav.fo.provider.rest;
+
+import no.nav.brukerdialog.security.context.InternbrukerSubjectHandler;
+import no.nav.fo.service.PepClientInterface;
+import no.nav.fo.service.VirksomhetEnhetService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import javax.ws.rs.core.Response;
+
+import static java.lang.System.setProperty;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class EnhetControllerTest {
+
+    @Mock
+    VirksomhetEnhetService virksomhetEnhetService;
+
+    @Mock
+    PepClientInterface pepClientInterface;
+
+    @InjectMocks
+    EnhetController enhetController;
+
+    @Before
+    public void setup() {
+        setProperty("no.nav.modig.core.context.subjectHandlerImplementationClass", InternbrukerSubjectHandler.class.getName());
+        InternbrukerSubjectHandler.setVeilederIdent("testident");
+        System.clearProperty("portefolje.pilot.enhetliste");
+    }
+
+    @Test
+    public void skalReturnereTomResponsNaarEnhetIkkeErIPilot() throws Exception {
+        setProperty("portefolje.pilot.enhetliste", "0000,0001");
+
+        Response response = enhetController.hentRessurser("0002");
+        verify(pepClientInterface, never()).isSubjectMemberOfModiaOppfolging(any());
+    }
+
+    @Test
+    public void skalIkkeReturnereTomResponsNaarPilotlisteIkkeInneholderEnhet() {
+        setProperty("portefolje.pilot.enhetliste", "[]");
+        Response response = enhetController.hentRessurser("0002");
+
+        verify(pepClientInterface, times(1)).isSubjectMemberOfModiaOppfolging(any());
+
+    }
+
+
+
+}
