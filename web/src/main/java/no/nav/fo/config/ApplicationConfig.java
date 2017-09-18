85eed744b6ef296cc03f5b5e0042f955d8b56d48
K149333 <mathilde.kirkhus@nav.no>
2017-09-18 15:51:42 +0200

    Lagt til api-app

--- web/src/main/java/no/nav/fo/config/ApplicationConfig.java
+++ web/src/main/java/no/nav/fo/config/ApplicationConfig.java
@@ -1,5 +1,6 @@
 package no.nav.fo.config;
 
+import no.nav.apiapp.ApiApplication;
 import no.nav.fo.internal.PingConfig;
 import no.nav.fo.service.PepClient;
 import no.nav.fo.service.PepClientInterface;
@@ -7,6 +8,8 @@ import no.nav.sbl.dialogarena.common.abac.pep.context.AbacContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+
+import static no.nav.apiapp.ApiApplication.Sone.FSS;
 
 @Configuration
 @Import({
@@ -16,9 +19,13 @@ import org.springframework.context.annotation.Import;
         AbacContext.class,
         PingConfig.class
 })
-public class ApplicationConfig {
+public class ApplicationConfig implements ApiApplication {
 
     @Bean
     public PepClientInterface pepClient() { return new PepClient(); }
 
+    @Override
+    public Sone getSone() {
+        return FSS;
+    }
 }
