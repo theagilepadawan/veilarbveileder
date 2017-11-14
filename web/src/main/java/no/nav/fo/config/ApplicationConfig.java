971a39d3d3d380d7802eb432defa5636e9a96965
Peder Korsveien <peder.korsveien@nav.no>
2017-11-14 09:01:17 +0100

    Fjern forwarding-servlet

--- web/src/main/java/no/nav/fo/config/ApplicationConfig.java
+++ web/src/main/java/no/nav/fo/config/ApplicationConfig.java
@@ -1,7 +1,6 @@
 package no.nav.fo.config;
 
 import no.nav.apiapp.ApiApplication;
-import no.nav.apiapp.ServletUtil;
 import no.nav.fo.provider.rest.RestConfig;
 import no.nav.fo.service.PepClient;
 import no.nav.fo.service.PepClientInterface;
@@ -11,10 +10,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-
-import javax.servlet.*;
-import javax.servlet.http.HttpServletRequest;
-import java.io.IOException;
 
 @Configuration
 @Import({
@@ -35,27 +30,4 @@ public class ApplicationConfig implements ApiApplication {
     public Sone getSone() {
         return Sone.FSS;
     }
-
-    @Override
-    public void startup(ServletContext servletContext) {
-        forwardTjenesterTilApi(servletContext);
-    }
-
-    // Bakoverkompatibilitet for klienter som går mot /tjenester
-    public static void forwardTjenesterTilApi(ServletContext servletContext) {
-        GenericServlet genericServlet = new GenericServlet() {
-            @Override
-            public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
-                HttpServletRequest request = (HttpServletRequest) req;
-                String requestURI = request.getRequestURI();
-                String relativPath = requestURI.substring(request.getContextPath().length() + request.getServletPath().length() + 1);
-                String apiPath = DEFAULT_API_PATH + relativPath;
-                LOGGER.warn("bakoverkompatibilitet: {} -> {} : {}", requestURI, apiPath, request.getHeader("Referer"));
-                RequestDispatcher requestDispatcher = req.getRequestDispatcher(apiPath);
-                requestDispatcher.forward(req, res);
-            }
-        };
-        ServletUtil.leggTilServlet(servletContext, genericServlet, "/tjenester/*");
-    }
-
 }
