582032f1d2f6151e6880e884d7f1e81873278582
Oscar Eckbo <oscar.eckbo@nav.no>
2018-04-13 14:47:24 +0200

    [FO-125] Legger til sjekk mot AD-rolle modia admin

--- web/src/main/java/no/nav/fo/config/ServiceConfig.java
+++ web/src/main/java/no/nav/fo/config/ServiceConfig.java
@@ -1,5 +1,6 @@
 package no.nav.fo.config;
 
+import no.nav.fo.service.LdapService;
 import no.nav.fo.service.VirksomhetEnhetService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,5 +11,10 @@ public class ServiceConfig {
     @Bean
     public VirksomhetEnhetService virksomhetEnhetServiceImpl() {
         return new VirksomhetEnhetService();
+    }
+
+    @Bean
+    public LdapService ldapService() {
+        return new LdapService();
     }
 }
---
+++ web/src/main/java/no/nav/fo/service/LdapService.java
@@ -0,0 +1,56 @@
+package no.nav.fo.service;
+
+import no.nav.fo.config.LdapContextProvider;
+
+import javax.naming.NamingEnumeration;
+import javax.naming.NamingException;
+import javax.naming.directory.SearchControls;
+import javax.naming.directory.SearchResult;
+import javax.naming.ldap.LdapContext;
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.lang.System.getProperty;
+import static no.nav.fo.service.ADRolleParserKt.parseADRolle;
+
+public class LdapService {
+
+    public boolean veilederHarRolle(String ident, String rolle) {
+        NamingEnumeration<SearchResult> result = sokLDAP(ident);
+        return getRoller(result).contains(rolle);
+    }
+
+    private static LdapContext ldapContext() {
+        return LdapContextProvider.getInitialLdapContext();
+    }
+
+    private NamingEnumeration<SearchResult> sokLDAP(String ident) {
+        String searchbase = "OU=Users,OU=NAV,OU=BusinessUnits," + getProperty("ldap.basedn");
+        SearchControls searchCtrl = new SearchControls();
+        searchCtrl.setSearchScope(SearchControls.SUBTREE_SCOPE);
+
+        try {
+            return ldapContext().search(searchbase, String.format("(&(objectClass=user)(CN=%s))", ident), searchCtrl);
+        } catch (NamingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private List<String> getRoller(NamingEnumeration<SearchResult> result) {
+        try {
+            NamingEnumeration<?> attributes = result.next().getAttributes().get("memberof").getAll();
+            return parseRollerFraAD(attributes);
+        } catch (NamingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private List<String> parseRollerFraAD(NamingEnumeration<?> attributes) throws NamingException {
+        List<String> rawRolleStrenger = new ArrayList<>();
+        while (attributes.hasMore()) {
+            rawRolleStrenger.add((String) attributes.next());
+        }
+        return parseADRolle(rawRolleStrenger);
+    }
+
+}
