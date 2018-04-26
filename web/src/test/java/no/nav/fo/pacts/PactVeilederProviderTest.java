f1245401b9f2a9efd63e58f934ff57476c4a1f16
Dervis Mansuroglu <dervis4@gmail.com>
2018-04-26 14:25:06 +0200

    PUS-155 Provider test for veilederinfo.

---
+++ web/src/test/java/no/nav/fo/pacts/PactVeilederProviderTest.java
@@ -0,0 +1,32 @@
+package no.nav.fo.pacts;
+
+import au.com.dius.pact.provider.junit.Consumer;
+import au.com.dius.pact.provider.junit.Provider;
+import au.com.dius.pact.provider.junit.State;
+import no.nav.pact.runner.NavHttpsPactTest;
+import no.nav.pact.runner.NavPactRunner;
+import org.junit.runner.RunWith;
+
+import java.util.Optional;
+
+@RunWith(NavPactRunner.class)
+@Provider("veilarbveileder")
+@Consumer("veilarbportefolje")
+public class PactVeilederProviderTest extends NavHttpsPactTest {
+
+    @State("a request for info about a veileder")
+    public void verifyProviderStateSinglePersonNoChildren() {
+        System.out.println("Known veileder.");
+    }
+
+    @State("a request for info about an unknown veileder")
+    public void verifyProviderStateNoData() {
+        System.out.println("Unknown veileder.");
+    }
+
+    @Override
+    public String getHttpTarget() {
+        return Optional.ofNullable(System.getenv("PACT_TARGET_URL")).orElse("https://app-t6.adeo.no");
+    }
+
+}
