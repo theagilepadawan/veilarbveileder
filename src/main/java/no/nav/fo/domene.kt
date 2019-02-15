2bb0b81cdf1405311002af5128138920511edaab
Jan Berge Ommedal <jan.berge.ommedal@nav.no>
2019-02-15 15:01:23 +0100

    KAIZEN-0 gjør fornavn og etternavn optional siden disse ikke er garantert tilstede

--- src/main/java/no/nav/fo/domene.kt
+++ src/main/java/no/nav/fo/domene.kt
@@ -3,7 +3,7 @@ package no.nav.fo
 import no.nav.virksomhet.organisering.enhetogressurs.v1.Enhet
 
 data class VeiledereResponse(val enhet: Enhet, val veilederListe: List<Veileder>)
-data class Veileder(val ident: String, val navn: String, val fornavn: String, val etternavn: String)
+data class Veileder(val ident: String, val navn: String, val fornavn: String?, val etternavn: String?)
 data class EnheterResponse(val veileder: Veileder, val enhetliste: List<PortefoljeEnhet>)
 data class PortefoljeEnhet(val enhetId: String, val navn: String)
 data class IdentOgEnhetliste(val ident: String, val enhetliste: List<PortefoljeEnhet>)
