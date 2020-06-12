da848d4ab6dbf231088c7901e4990cef44062564
alexandraback <alexandra1.back@gmail.com>
2020-06-12 10:46:36 +0200

    KAIZEN-0: Legg til sjekk hvis har MODIA_ADMIN_ROLLE

--- src/main/java/no/nav/veilarbveileder/service/AuthService.java
+++ src/main/java/no/nav/veilarbveileder/service/AuthService.java
@@ -3,6 +3,7 @@ package no.nav.veilarbveileder.service;
 import no.nav.common.abac.Pep;
 import no.nav.common.auth.subject.SsoToken;
 import no.nav.common.auth.subject.SubjectHandler;
+import no.nav.veilarbveileder.client.LdapClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -12,10 +13,14 @@ import org.springframework.web.server.ResponseStatusException;
 public class AuthService {
 
     private final Pep veilarbPep;
+    private final LdapClient ldapClient;
+
+    private static final String ROLLE_MODIA_ADMIN = "0000-GA-Modia_Admin";
 
     @Autowired
-    public AuthService(Pep veilarbPep) {
+    public AuthService(Pep veilarbPep, LdapClient ldapClient) {
         this.veilarbPep = veilarbPep;
+        this.ldapClient = ldapClient;
     }
 
     public String getInnloggetVeilederIdent() {
@@ -38,7 +43,8 @@ public class AuthService {
     }
 
     public void tilgangTilEnhet(String enhetId) {
-        if (!veilarbPep.harVeilederTilgangTilEnhet(getInnloggetVeilederIdent(), enhetId)) {
+        String ident = getInnloggetVeilederIdent();
+        if (!ldapClient.veilederHarRolle(ident, ROLLE_MODIA_ADMIN) && !veilarbPep.harVeilederTilgangTilEnhet(ident, enhetId)) {
             throw new ResponseStatusException(HttpStatus.FORBIDDEN);
         }
     }
