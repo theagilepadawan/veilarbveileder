9f91223783eb50dd27a98bde78855130cfa63e70
Dragutin Vujovic <dragutin.vujovic@nav.no>
2020-09-21 16:56:00 +0200

    Small bugfix

--- src/main/java/no/nav/veilarbveileder/service/AuthService.java
+++ src/main/java/no/nav/veilarbveileder/service/AuthService.java
@@ -39,7 +39,7 @@ public class AuthService {
     }
 
     public void sjekkTilgangTilOppfolging() {
-        if (veilarbPep.harTilgangTilOppfolging(getInnloggetBrukerToken())) {
+        if (!veilarbPep.harTilgangTilOppfolging(getInnloggetBrukerToken())) {
             throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Ikke tilgang til oppfølging");
         }
     }
