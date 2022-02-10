4fcef5b991a22be700b0893aa62dbdc732240569
stianStensli <stian.horn.stensli@nav.no>
2022-02-10 14:09:29 +0100

    Legger til muligheten for manuell endring av cache

--- src/main/java/no/nav/veilarbveileder/config/CacheConfig.java
+++ src/main/java/no/nav/veilarbveileder/config/CacheConfig.java
@@ -12,26 +12,7 @@ import java.util.concurrent.TimeUnit;
 @Configuration
 @EnableCaching
 public class CacheConfig {
-
-    public static final String HENT_ENHET_INFO_CACHE_NAME = "hentEnhetInfoCache";
-    public static final String VEILEDER_INFO_CACHE_NAME = "hentVeilederInfoCache";
     public static final String VEILEDER_ROLLE_CACHE_NAME = "veilederRolleCache";
-
-    @Bean
-    public Cache enhetInfoCache() {
-        return new CaffeineCache(HENT_ENHET_INFO_CACHE_NAME, Caffeine.newBuilder()
-                .expireAfterWrite(12, TimeUnit.HOURS)
-                .maximumSize(1000)
-                .build());
-    }
-
-    @Bean
-    public Cache veilederInfoCache() {
-        return new CaffeineCache(VEILEDER_INFO_CACHE_NAME, Caffeine.newBuilder()
-                .expireAfterWrite(12, TimeUnit.HOURS)
-                .maximumSize(10_000)
-                .build());
-    }
 
     @Bean
     public Cache veilederRolleCache() {
